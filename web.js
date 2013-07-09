bd9fa8d62e3d12c88f649007065784ed0a0f0fda
Deepak Sharma <deepak.coral@gmail.com>
2013-07-09 02:26:50 +0000

    back to ori

--- web.js
+++ web.js
@@ -1,18 +1,3 @@
-r express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
- var fs = require('fs');
- var buf = new Buffer(fs.readFileSync("index.html");
- response.send(buf.toString());
-  
- });
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
 var express = require('express');
 
 var app = express.createServer(express.logger());
