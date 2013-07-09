cb014443cb918046e16eb9199a725bc408b5717e
Deepak Sharma <deepak.coral@gmail.com>
2013-07-09 02:35:51 +0000

    aother

--- web.js
+++ web.js
@@ -4,7 +4,6 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buffer = new Buffer();
   response.send(buffer.toString(fs.readFileSync("index.html")));
   
 });
