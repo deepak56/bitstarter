3ed959890325f7eef00316a3ff7ea7399436e828
Deepak Sharma <deepak.coral@gmail.com>
2013-07-09 02:12:06 +0000

    Changed hello to read from index

--- web.js
+++ web.js
@@ -3,8 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+ var fs = require('fs');
+ var buf = new Buffer(fs.readFileSync('index.html');
+ response.send(buf.toString());
+  
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
